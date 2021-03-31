807e6e15e446944ac891fe0f791b329724b8f463
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2021-03-31 00:17:00 -0300

    adicionando alguns comentários

--- T2/romano.cpp
+++ T2/romano.cpp
@@ -4,12 +4,14 @@
 #include <map>
 #include "romano.hpp"
 
+// Valida uma string representando um número romano.
 bool Validate(std::string roman_number){
     return true;
 }
 
+// Converte algarismos romanos em algarismos arábico, 
 int ConvertRomanToArabic(std::string roman_number){
-    const int INVALID = -1;
+    const int INVALID = -1;  // constante de erro
     if(!Validate(roman_number))
         return INVALID;
 
@@ -20,12 +22,14 @@ int ConvertRomanToArabic(std::string roman_number){
                                      {'C', 100},
                                      {'D', 500},
                                      {'M', 1000}};
-    int arabic_number = 0;
+    int arabic_number = 0;  // resposta final
     int roman_number_len = roman_number.size();
 
     for(int i = 0; i < roman_number_len; i++){
+        // Caso o algarismo da frente seja menor, esse algarismo é somado.
         if(map_roman[roman_number[i+1]] <= map_roman[roman_number[i]])
-            arabic_number += map_roman[roman_number[i]]; 
+            arabic_number += map_roman[roman_number[i]];
+        // Caso contrário ele é subtraído.
         else
             arabic_number -= map_roman[roman_number[i]];  
     }
