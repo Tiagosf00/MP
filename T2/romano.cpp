de918611b0973384a3c02a83b9e2ac0b2acb73ea
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2021-03-31 00:12:37 -0300

    adicionando código de conversão (passou em todos os testes)

--- T2/romano.cpp
+++ T2/romano.cpp
@@ -1,12 +1,34 @@
 // Copyright [2020] <Tiago de Souza Fernandes>
 #include <iostream>
 #include <string>
+#include <map>
 #include "romano.hpp"
 
 bool Validate(std::string roman_number){
-    return (roman_number=="teste");
+    return true;
 }
 
 int ConvertRomanToArabic(std::string roman_number){
-    return Validate(roman_number);
+    const int INVALID = -1;
+    if(!Validate(roman_number))
+        return INVALID;
+
+    std::map<char, int> map_roman = {{'I', 1},
+                                     {'V', 5},
+                                     {'X', 10},
+                                     {'L', 50},
+                                     {'C', 100},
+                                     {'D', 500},
+                                     {'M', 1000}};
+    int arabic_number = 0;
+    int roman_number_len = roman_number.size();
+
+    for(int i = 0; i < roman_number_len; i++){
+        if(map_roman[roman_number[i+1]] <= map_roman[roman_number[i]])
+            arabic_number += map_roman[roman_number[i]]; 
+        else
+            arabic_number -= map_roman[roman_number[i]];  
+    }
+
+    return arabic_number;
 }
